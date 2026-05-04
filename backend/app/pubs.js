7ee7ca857a5158633d9f54b77c6146c08e809d11
elph000000 <106381126+elph000000@users.noreply.github.com>
2026-05-04 16:52:15 +0200

    altri commenti

--- backend/app/pubs.js
+++ backend/app/pubs.js
@@ -3,6 +3,7 @@ import Pub from './models/pub.js'
 import User from './models/user.js'
 const router = express.Router();
 
+//aggiungere qui le route per le pubblicazioni
 
 router.get('/', (req, res) => {
     res.send("Funziona!");
--- backend/app/users.js
+++ backend/app/users.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from './models/user.js'
 const router = express.Router();
 
+//aggiungere qui le route per le pubblicazioni
 
 router.get('/', (req, res) => {
     res.send("Funziona!");
