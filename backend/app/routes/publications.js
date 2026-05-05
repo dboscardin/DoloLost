c3a5bcaa5737aff92e5b87ae7bbdf5c7967919a8
Massimiliano Narminio <m.narminio@studenti.unitn.it>
2026-05-05 20:17:04 +0200

    Aggiunte routes pubblicazione: GET collezione + GET attive

--- backend/app/routes/publications.js
+++ backend/app/routes/publications.js
@@ -5,9 +5,30 @@ const router = express.Router();
 
 //aggiungere qui le route per le pubblicazioni
 
-router.get('/', (req, res) => {
-    res.send("Funziona!");
+router.use('/', async (req, res, next) => {
+    let pubs = Publication.find();
+    if(!pubs){
+        res.status(404).send();
+        console.log("Nessuna pubblicazione trovata");
+        return;
+    }
+    req["pubs"] = pubs;
+    console.log("Use Pubblicazioni");
+    next();
 });
 
+router.get('/', async (req, res) => {
+    let pubs = await req["pubs"].exec();
+    console.log("get pubblicazioni");
+    res.status(200).json(pubs);
+    return;
+});
+
+router.get('/attive', async(req, res) => {
+    let pubs = await req["pubs"].where('state').equals('unresolved').exec();
+    console.log("get attive");
+    res.status(200).json(pubs);
+    return;
+});
 
 export default router;
