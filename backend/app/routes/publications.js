387f76fbfb206cde5aa2d57d96084505e005ff42
Massimiliano Narminio <m.narminio@studenti.unitn.it>
2026-05-06 11:13:55 +0200

    JOIN route pubblicazioni con User

--- backend/app/routes/publications.js
+++ backend/app/routes/publications.js
@@ -6,27 +6,27 @@ const router = express.Router();
 //aggiungere qui le route per le pubblicazioni
 
 router.use('/', async (req, res, next) => {
-    let pubs = Publication.find();
+    let pubs = Publication.find().populate('user');
     if(!pubs){
         res.status(404).send();
         console.log("Nessuna pubblicazione trovata");
         return;
     }
     req["pubs"] = pubs;
-    console.log("Use Pubblicazioni");
+    //console.log("Use Pubblicazioni");
     next();
 });
 
 router.get('/', async (req, res) => {
     let pubs = await req["pubs"].exec();
-    console.log("get pubblicazioni");
+    //console.log("get pubblicazioni");
     res.status(200).json(pubs);
     return;
 });
 
 router.get('/attive', async(req, res) => {
     let pubs = await req["pubs"].where('state').equals('unresolved').exec();
-    console.log("get attive");
+    //console.log("get attive");
     res.status(200).json(pubs);
     return;
 });
