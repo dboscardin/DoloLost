0fbe9b9f50c4e0bf7590ba1102ca00ac46e5c64d
Massimiliano Narminio <m.narminio@studenti.unitn.it>
2026-05-06 17:10:01 +0200

    Pagina Frontend UserLogin, TODO: implementare chiamata api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './UserLogin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
