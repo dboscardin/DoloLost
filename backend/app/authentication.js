8b05e7e07ad88b52c369bd8974e2317f0dac150d
elph000000 <106381126+elph000000@users.noreply.github.com>
2026-05-06 16:18:13 +0200

    bugfix

--- backend/app/authentication.js
+++ backend/app/authentication.js
@@ -13,12 +13,12 @@ router.post('', async function(req, res) {
     if (user.password!=req.body.password) return res.status(401).json({success:false,message:'Wrong password'})
     // user authenticated -> create a token
     //dati criptati
-    var payload = { username: user.username, id: user._id, role: user.role , other_data: encrypted_in_the_token }
+    var payload = { username: user.username, id: user._id, role: user.role }
     var options = { expiresIn: 86400 } // expires in 24 hours
     var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
     //dati in chiaro
     res.json({ success: true, message: 'Enjoy your token!',
-        token: token, username: user.username, name: user.name, id: user._id, role: user.role , self: "api/v1/" + user._id
+        token: token, username: user.username, name: user.name, id: user._id, role: user.role /*, self: "api/v1/" + user._id*/
     });
 });
 export default router;
