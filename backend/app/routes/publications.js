77f63390f6723d508debd38a87b0b4090930e31a
elph000000 <106381126+elph000000@users.noreply.github.com>
2026-05-08 15:12:58 +0200

    documentazione

--- backend/app/routes/publications.js
+++ backend/app/routes/publications.js
@@ -17,12 +17,6 @@ router.use('/', async (req, res, next) => {
     next();
 });
 
-router.get('/', async (req, res) => {
-    let pubs = await req["pubs"].exec();
-    //console.log("get pubblicazioni");
-    res.status(200).json(pubs);
-    return;
-});
 
 //paramentri per filtrare:
 //Description (stringa contenuta in description) -> description
@@ -31,6 +25,46 @@ router.get('/', async (req, res) => {
 //data -> prima di -> date_before
 //tipologia (lost o found) -> type
 //posizione (sprint 2)
+
+router.get('/', async (req, res) => {
+    let query = req["pubs"];
+
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const params = url.searchParams;
+    const description = params.get('description');
+    const category = params.get('category');
+    const date_from = params.get('date_from');
+    const date_before = params.get('date_before');
+    const type = params.get('type');
+
+
+    if(description){
+        query = query.where('description').regex(new RegExp(description, 'i'));
+    }
+
+    if(category)
+    {
+        query = query.where('category').equals(category);
+    }
+
+    if (type) {
+        query = query.where('type').equals(type);
+    }
+
+    if (date_from) {
+        query = query.where('date').gte(new Date(date_from));
+    }
+
+    if (date_before) {
+        query = query.where('date').lte(new Date(date_before));
+    }
+
+    let pubs = await query.exec();
+    res.status(200).json(pubs);
+    return;
+});
+
+
 router.get('/attive', async(req, res) => {
     //let pubs = await req["pubs"].where('state').equals('unresolved').exec();
     let query = req["pubs"].where('state').equals('unresolved');
@@ -62,7 +96,7 @@ router.get('/attive', async(req, res) => {
     if (date_from) {
         query = query.where('date').gte(new Date(date_from));
     }
-    
+
     if (date_before) {
         query = query.where('date').lte(new Date(date_before));
     }
